6302264a7fbd7c1bc77479b40bb49346f7ab8e47
YogiAi <ayb854@163.com>
2015-10-12 22:49:32 +0800

    overall finished

--- src/objects3D/Cylinder.java
+++ src/objects3D/Cylinder.java
@@ -20,12 +20,14 @@ public class Cylinder {
 		for (float i = 0.0f; i < nSegments; i += 1.0f) {
 			float angle = PI * i * 2.0f / nSegments;
 			float nextAngle = PI * (i + 1.0f) * 2.0f / nSegments;
-			float x1 = (float) Math.sin(angle), y1 = (float) Math.cos(angle);
-			float x2 = (float) Math.sin(nextAngle), y2 = (float) Math
-					.cos(nextAngle);
+			float x1 = (float) Math.sin(angle) * radius, y1 = (float) Math
+					.cos(angle) * radius;
+			float x2 = (float) Math.sin(nextAngle) * radius, y2 = (float) Math
+					.cos(nextAngle) * radius;
 
 			// upper
-			glNormal(new Vector4f(x1, y1, 0.0f, 0.0f), new Vector4f(x2, y2, 0.0f, 0.0f));
+			glNormal(new Vector4f(x1, y1, 0.0f, 0.0f), new Vector4f(x2, y2,
+					0.0f, 0.0f));
 			GL11.glVertex3f(0.0f, 0.0f, 0.0f);
 			GL11.glVertex3f(x1, y1, 0.0f);
 			GL11.glVertex3f(x2, y2, 0.0f);
@@ -34,9 +36,9 @@ public class Cylinder {
 			GL11.glNormal3f(x1, y1, 0.0f);
 			GL11.glVertex3f(x1, y1, 0.0f);
 			GL11.glNormal3f(x2, y2, 0.0f);
-			GL11.glVertex3f(x2, y2, 1.0f);
+			GL11.glVertex3f(x2, y2, height);
 			GL11.glNormal3f(x1, y1, 0.0f);
-			GL11.glVertex3f(x1, y1, 1.0f);
+			GL11.glVertex3f(x1, y1, height);
 
 			// down triangle
 			GL11.glNormal3f(x1, y1, 0.0f);
@@ -44,10 +46,11 @@ public class Cylinder {
 			GL11.glNormal3f(x2, y2, 0.0f);
 			GL11.glVertex3f(x2, y2, 0.0f);
 			GL11.glNormal3f(x2, y2, 0.0f);
-			GL11.glVertex3f(x2, y2, 1.0f);
+			GL11.glVertex3f(x2, y2, height);
 
 			// down
-			glNormal(new Vector4f(x1, y1, 1.0f, 0.0f), new Vector4f(x2, y2, 1.0f, 0.0f));
+			glNormal(new Vector4f(x1, y1, height, 0.0f), new Vector4f(x2, y2,
+					height, 0.0f));
 			GL11.glVertex3f(0.0f, 0.0f, 1.0f);
 			GL11.glVertex3f(x1, y1, 1.0f);
 			GL11.glVertex3f(x2, y2, 1.0f);
@@ -55,7 +58,7 @@ public class Cylinder {
 		GL11.glEnd();
 	}
 
-	private void glNormal(Vector4f vector1, Vector4f vector2){
+	private void glNormal(Vector4f vector1, Vector4f vector2) {
 		Vector4f normal = vector1.cross(vector2).normal();
 		GL11.glNormal3f(normal.x, normal.y, normal.z);
 	}
